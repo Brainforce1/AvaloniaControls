d7340d1ac9b40460356813cf9164d912a244ec6b
Giorgio Migliaccio <giorgio@brainforce.one>
2026-06-22 22:03:42 +0200

    Initial commit

--- AvaloniaControlTesterApp/obj/Debug/net10.0/AvaloniaControlTesterApp.AssemblyInfo.cs
+++ AvaloniaControlTesterApp/obj/Debug/net10.0/AvaloniaControlTesterApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvaloniaControlTesterApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d84c9bbff17c76037afad57314d61d944b101cf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvaloniaControlTesterApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvaloniaControlTesterApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
